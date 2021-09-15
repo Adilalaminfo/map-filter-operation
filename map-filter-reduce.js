035279d693896add249f07364f7b58ec576405cf
Adilalaminfo <mdadilalaminfo@gmail.com>
2021-09-15 11:11:40 +0530

    first commit

---
+++ map-filter-reduce.js
@@ -0,0 +1,13 @@
+   const arr=[3,2,1,5];
+
+/*function triple(x){
+    return x*3;
+} 
+//const output=arr.map( (x) => x.toString(2))
+function IsOdd(x){
+    return x%2;
+} */
+
+const output=arr.filter( (x)=> x<9);
+
+console.log(output);
